b84016d4e539ac31411542bb2062c22c215cc157
Jose Vitor <josevfsilveira@gmail.com>
2026-05-20 22:34:09 -0300

    Enhance PowerSystem and GaussSeidel classes with detailed docstrings; update add_Zline and add_bus_params methods to include parameter descriptions and improve Ybus calculation documentation.

--- gauss-seidel.py
+++ gauss-seidel.py
@@ -9,6 +9,11 @@ def R2P(x: complex):
 
 
 class PowerSystem:
+    '''
+    Classe que representa um sistema de potência.
+    As suas funções permitem definir os tipo de barras, as impedancias das linhas entre as barras, os parametros de potencia ativa e reativa das barras.
+    Também pode calcular a matriz Ybus do sistema e armazenar as tensoes iniciais das barras para o metodo de Gauss-Seidel.
+    '''
 
     def __init__(self, bus_types: list[str]) -> None:
 
@@ -34,7 +39,15 @@ class PowerSystem:
                 self.B2_lines[(i, j)] = 0 # Inicializar as susceptancias das linhas com zero para indicar que as linhas ainda nao foram definidas
 
     def add_Zline(self, bus1: int, bus2: int, Z: complex, B2: float=0) -> None:
-        
+        '''
+        Adiciona uma linha de impedância entre duas barras.
+
+        --> bus1: índice da primeira barra conectada pela linha.
+        --> bus2: índice da segunda barra conectada pela linha.
+        --> Z: impedância da linha em p.u. (valor complexo).
+        --> B2: metade susceptância (B/2) shunt da linha em p.u. (valor real).
+        '''
+
         if bus1 == bus2:
             raise ValueError("Não é possível adicionar uma linha entre a mesma barra. Verifique os índices das barras informados.")
         elif bus1 < 0 or bus1 >= self.num_buses or bus2 < 0 or bus2 >= self.num_buses:
@@ -45,6 +58,7 @@ class PowerSystem:
         bus_index = (bus1, bus2)
         mirrowed_bus_index = (bus2, bus1)
 
+        # Caso o indice informado seja valido, adicionar a impedancia da linha e a susceptancia shunt da linha nos dicionarios correspondentes
         if bus_index in self.Zlines or mirrowed_bus_index in self.Zlines:
             self.Zlines[bus_index] = Z
             self.B2_lines[bus_index] = B2
@@ -52,6 +66,15 @@ class PowerSystem:
             raise ValueError(f"Não existe linha entre as barras {bus1} e {bus2}.")
         
     def add_bus_params(self, bus: int, Vabs: float, Vangle: float, P: float=None, Q: float=None) -> None:
+        '''
+        Adiciona os parametros de tensao, potencia ativa e potencia reativa para uma barra do sistema de potencia.
+
+        --> bus: índice da barra para a qual os parâmetros estão sendo adicionados.
+        --> Vabs: magnitude da tensão da barra em p.u.
+        --> Vangle: ângulo da tensão da barra em graus.
+        --> P: potência ativa especificada para a barra em p.u. (obrigatório para barras do tipo PQ e PV).
+        --> Q: potência reativa especificada para a barra em p.u. (obrigatório para barras do tipo PQ).
+        '''
         
         # Adicionar os parametros de tensao das barras
         self.V0[bus] = P2R(Vabs, Vangle)
@@ -62,18 +85,27 @@ class PowerSystem:
                 raise ValueError(f"Para barras do tipo PQ, é necessário especificar tanto a potência ativa (P) quanto a potência reativa (Q).")
             self.Pespec[bus] = P
             self.Qespec[bus] = Q
+        
         elif self.bus_types[bus] == 'PV':
             if P is None:
                 raise ValueError(f"Para barras do tipo PV, é necessário especificar a potência ativa (P).")
             self.Pespec[bus] = P
-            self.Qespec[bus] = None  # Potencia reativa nao especificada para
+            self.Qespec[bus] = None  # Potencia reativa nao especificada para barra PV
+        
         elif self.bus_types[bus] == 'Slack':
             self.Pespec[bus] = None  # Potencia ativa nao especificada para barra slack
             self.Qespec[bus] = None  # Potencia reativa nao especificada para barra slack
+        
         else:
             raise ValueError(f"Tipo de barra inválido: {self.bus_types[bus]}. Tipos válidos são: ['PQ', 'PV', 'Slack']")
 
     def calculate_Ybus(self, verbose=False) -> None:
+        '''
+        Calcula a matriz Ybus do sistema de potência com base nas impedâncias das linhas entre as barras e nas susceptâncias shunt das linhas.
+        Armazena os resultados na variável Ybus da classe PowerSystem.
+
+        --> verbose: habilita a impressão da matriz Ybus calculada.
+        '''
 
         # Zerar a matriz Ybus antes de calcular os valores, para evitar que valores antigos sejam mantidos caso a função seja chamada mais de uma vez
         self.Ybus = np.zeros((self.num_buses, self.num_buses), dtype=complex)
@@ -83,12 +115,15 @@ class PowerSystem:
             if Zvalue == 0.0 + 0.0j:
                 raise ValueError("Impedância de linha não definida corretamente. Verifique as linhas adicionadas.")
 
+        # Calcular os elementos de Ybus fora da diagonal
         for bus1 in range(self.num_buses):
             for bus2 in range(self.num_buses):
                 if (bus1, bus2) in self.Zlines:
                     ybus = 1 / self.Zlines[(bus1, bus2)]
                     self.Ybus[bus1, bus2] = -ybus  # Elemento fora da diagonal e negativo
                     self.Ybus[bus2, bus1] = -ybus  # Elemento simetrico fora da diagonal
+
+        # Calcular os elementos de Ybus na diagonal
         for bus in range(self.num_buses):
             self.Ybus[bus, bus] = -np.sum(self.Ybus[bus, :])  # Elemento diagonal eh a soma dos elementos fora da diagonal, mas positivo
             self.Ybus[bus, bus] += np.sum([1j*B2 for (b1, b2), B2 in self.B2_lines.items() if bus in (b1, b2)])  # Adicionar as susceptancias shunt das linhas conectadas a barra
@@ -98,12 +133,29 @@ class PowerSystem:
 
 
 class GaussSeidel:
+    '''
+    Classe que implementa o método de Gauss-Seidel para resolver o fluxo de potência em um sistema de potência.
+    As suas funções permitem realizar as iterações do método de Gauss-Seidel.
+    Atualiza as tensões das barras a cada iteração e verificando a convergência com base em um critério de tolerância definido pelo usuário.
+    Também pode imprimir os resultados de cada iteração, incluindo as tensões calculadas para cada barra, em um sistema de coordenadas definido pelo usuário (polar ou retangular).
+    '''
+
     def __init__(self, sys: PowerSystem) -> None:
         self.sys = sys
-        self.V_iter = sys.V0.copy()  # Armazenar as tensoes anteriores para verificar a convergencia
+        # Array que armazena as tensoes das barras para as iteracoes do metodo de Gauss-Seidel, 
+        self.V_iter = sys.V0.copy()  # Inicializado com as tensoes iniciais definidas no sistema de potência
 
     @staticmethod
     def calc_new_V(bus: int, V_iter: np.ndarray, Ybus: np.ndarray, S_espec: complex) -> complex:
+        '''
+        Calcula a nova tensão para uma barra do sistema de potência com base nos argumentos passados pela função.
+
+        --> bus: índice da barra para a qual a nova tensão está sendo calculada.
+        --> V_iter: array contendo as tensões atuais das barras do sistema de potência.
+        --> Ybus: matriz de admitância do sistema de potência.
+        --> S_espec: potência especificada para a barra em p.u. (valor complexo, P + jQ).
+        '''
+
         new_V = np.conj(S_espec) / np.conj(V_iter[bus])  # Calcular a corrente injetada na barra
         new_V -= np.sum(Ybus[bus, :] * V_iter, where=(np.arange(Ybus.shape[0]) != bus) )  # Somar as contribuicoes das outras barras
         return new_V / Ybus[bus, bus]  # Dividir pela admitancia da barra para obter a nova tensao
@@ -112,8 +164,11 @@ class GaussSeidel:
     def iterate(self, tolerance: float=1e-4, max_iterations: int=100, verbose: bool=False, coordinate_system: str='polar') -> None:
         '''
         Realiza as iteracoes do metodo de Gauss-Seidel para o sistema de potencia fornecido, atualizando as tensoes das barras a cada iteracao.
-        Verbose habillita a impressao dos resultados de cada iteracao.
-        coordinate_system define o sistema de coordenadas para a impressao dos resultados, podendo ser 'polar' ou 'rectangular'.
+
+        --> tolerance: Define o criterio de convergencia, ou seja, a diferenca maxima entre as tensoes das barras em iteracoes consecutivas para considerar que o metodo convergiu.
+        --> max_iterations: Define o numero maximo de iteracoes a serem realizadas caso o metodo nao converja.
+        --> verbose: habillita a impressao dos resultados de cada iteracao.
+        --> coordinate_system: define o sistema de coordenadas para a impressao dos resultados, podendo ser 'polar' ou 'rectangular'.
         '''
 
         if verbose and coordinate_system not in ['polar', 'rectangular']:
