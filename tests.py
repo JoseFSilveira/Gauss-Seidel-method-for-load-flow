7c4c0113b319d823adcac05f97f8c9960937b33f
Jose Vitor <josevfsilveira@gmail.com>
2026-05-20 22:50:39 -0300

    Remove unused P2R function and associated test cases from tests.py

--- tests.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def P2R(abs: float, angle: float):
-    angle = np.deg2rad(angle)  # Converter angulo de graus para radianos
-    return abs*np.cos(angle) + 1j*abs*np.sin(angle)
-
-x = P2R(np.inf, 0)
-print(x)
